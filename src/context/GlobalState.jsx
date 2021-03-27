992b194521b854db796601c2a164ec9489243d14
unknown <industriousthams@gmail.com>
2021-03-27 14:57:20 +0530

    Initial Commit

---
+++ src/context/GlobalState.jsx
@@ -0,0 +1,47 @@
+import React, {createContext, useReducer} from 'react'
+import AppReducer from './AppReducer';
+
+// initial State
+const initialState={
+  transactions:[]
+}
+
+// Create context
+export const GlobalContext = createContext(initialState);
+
+// to get access to the global state we need to wrap all the 
+// component into provider component
+
+// Provider Component
+export const GlobalProvider = ({children}) =>{
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    // Actions (That make call to our  AppReducer)
+    function deleteTransaction(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        })
+    }
+
+    function addTransaction(transaction) {
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        })
+    }
+
+    return (
+        <GlobalContext.Provider value={{
+            transactions: state.transactions,
+            deleteTransaction,
+            addTransaction            
+        }}>
+            {children}
+        </GlobalContext.Provider>
+     
+    );
+
+}
+
+ 
